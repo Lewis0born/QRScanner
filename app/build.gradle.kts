613f1f4cccb5d0e9c871bace568760e5aa4f996c
Lewis0born <lewis.oborn@gmail.com>
2023-11-27 19:35:09 +1030

    Update compileSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.qrscanner"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.qrscanner"
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
